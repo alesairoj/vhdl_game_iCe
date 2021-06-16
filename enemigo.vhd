a87715c3857f46bebb437d50aa47caf39f046197
salas <salas@trajano.us.es>
2021-06-16 19:31:23 +0200

    Creado enemigo 2, disminuido tamaño enemigos.
    Ahora mismo no se muestran los dos enemigos a la vez en la
    pantalla. He creado una nueva instancia con nuevas señales para las
    coordenadas, y que el enemigo dos sea un offset del enemigo 1.

--- enemigo.vhd
+++ enemigo.vhd
@@ -17,6 +17,8 @@ architecture Behavioral of enemigo is
 	signal X, Y : unsigned (9 downto 0);
 	signal xini, yini : unsigned (9 downto 0);
 	signal xinivec, yinivec : STD_LOGIC_VECTOR ( 9 downto 0);
+--	signal x2vec, y2vec : STD_LOGIC_VECTOR ( 9 downto 0);
+	signal x2, y2 : unsigned (9 downto 0);
 	signal enctud : STD_LOGIC;
 
 	component ctud is
@@ -58,6 +60,26 @@ begin
                   Q => yinivec
           );
 
+--         enemigo2_xini: ctud
+--           generic map (Nbit => 10)
+--           port map (clk => clk,
+--                     reset => reset,
+--                     resets => '0',
+--                     enable => '1',
+--                     sentido => '1',
+--                     Q => x2vec
+--                     );
+--         enemigo2_yini: contador
+--           generic map (Nbit => 10)
+--           port map (clk => clk,
+--                     reset => reset,
+--                     resets => '0',
+--                     enable => '1',
+--                     Q => y2vec
+--                     );
+
+-- y2 <= unsigned(yinivec)+30;
+-- x2 <= unsigned(xinivec)+20;
 yini <= unsigned(yinivec);
 xini <= unsigned(xinivec)+260;
 	X <= unsigned(eje_x);
@@ -66,8 +88,8 @@ xini <= unsigned(xinivec)+260;
 	process(X, Y)
 	begin
 
-		if (X > xini) AND (X < (xini + 50)) AND (Y > yini) AND (Y < (yini + 50)) then
-			R <= '1'; G <= '1'; B <= '1';
+		if (X > xini) AND (X < (xini + 15)) AND (Y > yini) AND (Y < (yini + 15)) then
+			R <= '0'; G <= '0'; B <= '0';
 		else
 			R <= '1'; G <= '1'; B <= '0';
 		end if;
